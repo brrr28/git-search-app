4707c79ea13ea3ffa62bbeec310b9398b07dcce6
Pavel Bogachenko <73500211+brrr28@users.noreply.github.com>
2022-01-03 21:55:26 +0200

    Revert "Blocks page + layout table page"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import { BlocksComponent } from './components/blocks/blocks.component';
-import { TableComponent } from './components/table/table.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent,
-    BlocksComponent,
-    TableComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
